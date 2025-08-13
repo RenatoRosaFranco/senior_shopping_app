6890169201980cc296334fca3bf4ab3f1d420ac8
Renato Rosa Franco <renato_ny@live.com>
2025-08-12 22:43:01 -0300

    feature: test build pipeline.

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,3 +4,8 @@ pin "application"
 pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
 pin "bootstrap" # @5.3.7
 pin "bootstrap", to: "bootstrap.bundle.min.js"
+pin "@hotwired/turbo-rails", to: "@hotwired--turbo-rails.js" # @8.0.16
+pin "@hotwired/turbo", to: "@hotwired--turbo.js" # @8.0.13
+pin "@rails/actioncable/src", to: "@rails--actioncable--src.js" # @8.0.200
+pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.2
+pin_all_from "app/javascript/controllers", under: "controllers"
